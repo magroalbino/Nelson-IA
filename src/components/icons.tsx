2ae332c9a703ee2b2117c2f341b43fa38d4ff935
Yan Renat <yanrenat@gmail.com>
2025-09-21 04:51:56 +0000

    Corrija este alerta: Image with src "/assets/logo-eustaquio-2.png" has e

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
+import type { ImageProps } from "next/image";
 
-export const Logo = (props: { width?: number, height?: number, className?: string }) => (
+export const Logo = (props: Omit<ImageProps, 'src' | 'alt'>) => (
     <Image
         src="/assets/logo-eustaquio-2.png"
         alt="Eustáquio IA Logo"
-        width={props.width || 64}
-        height={props.height || 64}
-        className={props.className}
-        style={{ height: 'auto' }}
+        {...props}
         priority
     />
 );
